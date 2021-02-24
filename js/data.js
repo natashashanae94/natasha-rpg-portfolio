c614ff7b7bc25ef5d6720ef543e2759940f930bc
Natasha Johnson <natashashanae94@gmail.com>
2021-02-23 20:02:44 -0500

    first commit

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const houses = [
+    {name: 'About House', id:'#aboutHouse', width:247, height: 158, top: 450, left:20, door:{width: 46, height: 50, left: 56, top: 110}},
+    {name: 'Services House', id:'#serviceHouse', width: 180, height: 175, top: 701, right:50, door:{width: 48, height: 69, left: 37, bottom: 67}},
+    {name: 'Portfolio House', id:'#portfolioHouse', width:190, height:190, top: 950, left: 50, door:{width: 50, height: 36, bottom: 75, left: 84}},
+    {name: 'Blog House', id:'#blogHouse', width:200, height: 175, top: 1200, right: 100, door:{width:47, height:58, left:30, top:207}}
+];
+
+const roads = [
+    {name: 'About Road', id:'#aboutRoad', height: 200, top: 493, direction: "left"},
+    {name: 'Services Road', id:'#serviceRoad', height: 200, top: 779, direction: "right"},
+    {name: 'Portfolio Road', id:'#portfolioRoad', height: 200, top: 1037, direction: "left"},
+    {name: 'Blog Road', id: '#blogRoad', height: 200, top: 1259, direction: "right"}
+];
+
+const notifications = [
+    {
+        id: 1,
+        type: 'error',
+        text: 'You can\'t teleport here.'
+    }
+];
